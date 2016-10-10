04fafe0f4b006d1f74dcb56ef6c9c42e86113c68
LachlanD <lachlan.d@gmail.com>
2016-10-11 00:47:27 +1100

    Up to task 3

--- assignment3.R
+++ assignment3.R
@@ -42,11 +42,8 @@ random.group = sample(c("F","M"), size = 30, replace = T)
 random.group
 
 random.dge=DGEList(counts=counts[,1:30], group=factor(random.group))
-
 random.dge=calcNormFactors(random.dge)
-
 random.dge=estimateDisp(random.dge)
-
 r.et=exactTest(random.dge)
 
 r.tt = topTags(r.et, n=10, adjust.method="none", sort.by="P")
@@ -55,19 +52,20 @@ r.tt$table["Chromosome"]=counts[rownames(r.tt),][32]
 grid.table(r.tt$table)
 
 
-r.de = topTags(r.et, n=15, adjust.method="none", sort.by="P")
+r.de = topTags(r.et, n=15, adjust.method="BH", sort.by="P")
 
 heatmap(random.dge$counts[rownames(random.dge) %in% rownames(r.de),])
 
 r.ua =decideTestsDGE(r.et, adjust.method = "none",p.value = 0.05)
-r.bh = decideTestsDGE(r.et, adjust.method = "BH", p.value = 0.05)
 summary(r.ua)
 sum(r.ua!=0)
 sum(r.ua!=0)/length(r.ua)
+
+r.bh = decideTestsDGE(r.et, adjust.method = "BH", p.value = 0.05)
 summary(r.bh)
 sum(r.bh!=0)
 sum(r.bh!=0)/length(r.bh)
-S
+
 sum(r.bh[counts[32]=='Y'])
 sum(counts[32]=='Y')
 
